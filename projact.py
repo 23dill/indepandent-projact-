777f2389d0a3207ac6f378cc4f718a8b47249d6b
23dill <90460001+23dill@users.noreply.github.com>
2021-09-10 12:31:54 -0400

    Add files via upload

--- projact.py
+++ projact.py
@@ -1,3 +1,37 @@
 import pygame
+from sys import exit
+import os
 
-pygame.init()
+import physics_engan
+
+FPS = 100
+WIDTH, HIEGHT = 900, 500
+pygame.display.init()
+Win = pygame.display.set_mode((WIDTH, HIEGHT))
+pygame.display.set_caption("code for yet unnamed game")
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+def draw_screen():
+    
+
+    pygame.display.update()
+
+def main():
+
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(FPS)
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        
+
+
+
+if __name__ == "__main__":
+    main()
