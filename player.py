caacfd60ef3f0e7d3ddf87f1b8c3769c6e0a1efb
23dill <90460001+23dill@users.noreply.github.com>
2021-10-08 13:49:22 -0400

    Add files via upload

--- player.py
+++ player.py
@@ -8,9 +8,8 @@ pygame.display.init()
 Win = pygame.display.set_mode((WIDTH, HIEGHT))
 
 
-class Player(pygame.sprite.Sprite):
+class Player(object):
     def __init__(self):
-        super(Player, self).__init__()
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
